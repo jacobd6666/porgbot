1f4fadce463fe9fdaa34dd1f90e5b5edbc838014
jacobd6666 <75499608+jacobd6666@users.noreply.github.com>
2021-02-23 14:57:08 -0500

    Merged twassigner into Main

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 #importing dependencies
 import discord
 from discord.ext import commands
+import json
 #importing other functions
 from config import *
 from Gear_Find import *
@@ -9,7 +10,10 @@ from Char_Gear import *
 import pandas as pd
 import numpy as np
 #initialize the bot
-bot = commands.Bot(command_prefix=prefix)
+intents = discord.Intents.default()
+intents.members = True
+intents.messages = True
+bot = commands.Bot(command_prefix = prefix, intents=intents)
 
 #Do this when the bot runs
 @bot.event
@@ -48,5 +52,55 @@ async def gearsearch(ctx, *gearInput): #this time just one argument, the gear th
     gear = ''.join(charInput).lower() #concatenate and make lowercase
     await ctx.send(gear) #just return the name of teh gear they input. This command has not been started on
 
+def generateAssignments(assignemnts):
+    TWembed = discord.Embed(title = "Assignments - React to this when you have filled your assignments", color = 0x2F3136)
+    for name, value in assignments.items():
+        TWembed.add_field(name = f"{name}: {'✅' if value['assigned'] else '❌'}", value = value["teams"], inline = False)
+    return TWembed
+@bot.command(name='TWStart')
+@commands.has_role(730466266896269406) #checks if the user has the Porg Lords Member role. If not, the command doesn't run
+async def test(ctx):
+    global assignments
+    global AssignMessage
+    with open("assignments.json", "r") as fp:
+        global assignments 
+        assignments = json.load(fp)
+    TWembed = generateAssignments(assignments)
+    AssignMessage = await ctx.send(embed = TWembed)
+    
+@bot.event
+async def on_raw_reaction_add(payload):
+    global AssignMessage
+    global assignments
+
+    #make sure there actually is a message with assignments
+    if AssignMessage != None:
+        #check if the message that got reacted to was the message with assignments
+        if payload.message_id == AssignMessage.id:
+            #check which user with assignments reacted
+            for name, value in assignments.items():
+                if payload.user_id == value["discord_id"]:
+                    value["assigned"] = True
+                    print(f'{await bot.fetch_user(payload.user_id)} confirmed they deployed assignments.')
+                    break
+            TWembed = generateAssignments(assignments)
+            await AssignMessage.edit(embed = TWembed)
+
+async def on_raw_reaction_remove(payload):
+    global AssignMessage
+    global assignments
+
+    #make sure there actually is a message with assignments
+    if AssignMessage != None:
+        #check if the message that got reacted to was the message with assignments
+        if payload.message_id == AssignMessage.id:
+            #check which user with assignments reacted
+            for name, value in assignments.items():
+                if payload.user_id == value["discord_id"]:
+                    value["assigned"] = False
+                    print(f'{await bot.fetch_user(payload.user_id)} un-confirmed they deployed assignments.')
+                    break
+            TWembed = generateAssignments(assignments)
+            await AssignMessage.edit(embed = TWembed)
 
 bot.run(Token) #start the bot
