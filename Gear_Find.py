1513cc91affbbd2f4bbf3240dccea79c572d7014
Ken Darling <GuruGuy00@Gmail.com>
2021-03-10 17:11:13 -0500

    Moved some files

--- Gear_Find.py
+++ Gear_Find.py
@@ -2,10 +2,13 @@
 import pandas as pd
 
 def findGear(gearLocCsv, toon_csv):
+
     gearLoc = pd.read_csv(gearLocCsv)
     names = pd.read_csv(toon_csv)  
+
     ident_names = [(i,i) for i in gearLoc["ITEM"].unique()]
     ident_df = pd.DataFrame(ident_names, columns=["ITEM", "NICKNAME"])
+    
     names = names.append(ident_df).reset_index(drop=True)
     gearLoc = gearLoc.merge(names, on="ITEM", how="inner")
     
--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-Token = "censored"
-prefix = '+'
+Token = "Nzc5NTYxNDM4ODAwMjQ4ODMy.X7iVKg.bueuhqbXrNWgH883TRum-5CToPk"
+prefix = '?'
